0d91b3a8deffc18f51a08d27c6538315ea44286e
Sebastian Aristondo <ari20880@uvg.edu.gt>
2022-04-30 17:17:45 -0600

    Suscripciones

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,13 +9,23 @@ import Myvideos from '../Images/Mis videos.svg'
 import tarde from '../Images/tarde.svg'
 import liked from '../Images/Videos que me gustan.svg'
 import more from '../Images/showmore.svg'
+import btc from '../Images/BTC.jpg'
+import chuy from '../Images/Chuy.jpg'
+import enchufe from '../Images/enchufe.jpg'
+import sergio from '../Images/Sergio.jpg'
+/* import xn from '../Images/XN.jpg'
+import xifra from '../Images/Xifra.jpg'
+import grefg from '../Images/Grefg.jpg' */
 
 const menu = [['Explorar', explorar], ['Shorts', shorts], ['Suscripciones', sub]]
 const menu2 = [['Biblioteca', bib], ['Historial', Historial], ['Mis vídeos', Myvideos],
     ['Ver más tarde', tarde], ['Vídeos que me gustan', liked], ['Mostrar más', more]]
+const menu3 = [['Bitcoin al dia', btc], ['Chuy Almada', chuy], ['enchufetv', enchufe],
+    ['Sergio Peñuñuri', sergio]]/* , ['TheGrefg', grefg],
+['Xifra Lifestyle', xifra], ['Xifra Nation', xn]] */
 
 const Menu = () => (
-    <scroll className="Menu-Container">
+    <div className="Menu-Container">
         <div className="Boton-Container">
             <div className="Icon-container" style={{ backgroundImage: `url(${casa})` }} />
             <text>Inicio</text>
@@ -34,7 +44,14 @@ const Menu = () => (
             </div>
         ))}
         <hr />
-    </scroll>
+        <text className="titulo">SUSCRIPCIONES</text>
+        {menu3.map((obj) => (
+            <div className="Boton1-Container">
+                <div className="perfil-container" style={{ backgroundImage: `url(${obj[1]})` }} />
+                <text className="profile-name">{obj[0]}</text>
+            </div>
+        ))}
+    </div>
 )
 
 export default Menu
