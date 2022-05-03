4a3c61cc2e01e5448aaf6a4de341a67a3fbc75ae
Sebastian Aristondo <ari20880@uvg.edu.gt>
2022-05-03 09:33:45 -0600

    Carrusel ajustar texto

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,17 +1,21 @@
 import './Carrusel.css'
 
-const opciones = ['Barra', 'Equimapiento de ejercicio', 'Hollow Knight', 'Clash Royale', 'Mixes', 'El Rubius', 'Jedis'] /* ,
+const opciones = ['Barra', 'Equimapiento de ejercicio', 'Hollow Knight', 'Clash Royale', 'Mixes', 'El Rubius', 'Jedis',
     'Calistenia', 'Cine de ciencia ficción', 'Arte', 'Musica', 'Bitcoin', 'Franco Escamilla',
     'Personajes', 'Programación', 'Videojuego de sigilo', 'Subidas recientes',
-    'En directo', 'Visto', 'Novedad para ti'] */
+    'En directo', 'Visto', 'Novedades para ti']
 const Carrusel = () => (
     <div>
         <hr className="hr-carrusel" />
         <div className="Contenido">
-            <div className="boton-carruselw">Todo</div>
+            <div className="boton-carruselw">
+                <div className="cat1">Todo</div>
+            </div>
             {
                 opciones.map((op) => (
-                    <div className="boton-carruselb">{op}</div>
+                    <div className="boton-carruselb">
+                        <div className="cat">{op}</div>
+                    </div>
                 ))
 
             }
