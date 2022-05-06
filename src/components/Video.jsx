28aeb32722a4c0f6d48b24fbd401a8add4f70d15
Sebastian Aristondo <ari20880@uvg.edu.gt>
2022-05-05 18:26:43 -0600

    hover carrusel

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,19 +1,27 @@
 import './Video.css'
 import videosYT from '../../public/videos.json'
 
-console.log(videosYT)
-
 const Video = () => (
     <div className="video-contenedor">
         {
             videosYT.map((video) => (
-                <div className="video-contenido">
-                    <div key={video.titulo} className="video-imagen" style={{ backgroundImage: `url(${video.imagen})` }}>
+                <div key={video.titulo} className="video-contenido">
+                    <div className="video-imagen" style={{ backgroundImage: `url(${video.imagen})` }}>
                         <div className="time">
                             <div className="texto-time">{video.tiempo}</div>
                         </div>
                     </div>
-                    <div className="perfilimagen" style={{ backgroundImage: `url(${video.perfil})` }} />
+                    <div className="footer-videos">
+                        <div className="perfilimagen" style={{ backgroundImage: `url(${video.perfil})` }} />
+                        <div className="footer-texto">
+                            <div className="titulo-video">{video.titulo}</div>
+                            <div className="footer-videos">
+                                <div className="creador-contenido">{video.creador}</div>
+                                <div className="verificatedimg" />
+                            </div>
+                            <div className="vistas-contenido">{video.vistas}</div>
+                        </div>
+                    </div>
                 </div>
             ))
         }
